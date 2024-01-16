62a2efdad861d9e23aad95a408dc4762eb371f49
A1bert04 <albertbru04@gmail.com>
2024-01-16 10:16:26 +0100

    Add delete route for question sets

---
+++ backend/routes/sets/deleteSet.ts
@@ -0,0 +1,33 @@
+import {Request, Response} from "express";
+import getDb from "../../prisma/db";
+import { deleteSetResponse } from "../../schemas/sets/deleteSetResponse";
+
+export async function handleDeleteQuestionSet(req: Request, res: Response) {
+    const db = await getDb();
+
+    const set = await db.questionSet.findUnique({
+        where: {
+            id: parseInt(req.params.id)
+        }
+    });
+
+    if (!set || set.ownerId !== req.user!.id) {
+        const response: deleteSetResponse = {
+            success: false,
+            message: "Set not found",
+        };
+        return res.status(404).json(response);
+    }
+
+    await db.questionSet.delete({
+        where: {
+            id: parseInt(req.params.id)
+        }
+    });
+    
+    const response: deleteSetResponse = {
+        success: true,
+        message: "Set deleted successfully",
+    };
+    return res.status(200).json(response);
+}
--- backend/routes/sets/router.ts
+++ backend/routes/sets/router.ts
@@ -3,6 +3,7 @@ import authMiddleware from '../../middleware/auth';
 import { handleGetMySets, handleGetSetById } from './getSet';
 import { handleCreateQuestionSet } from './createSet';
 import { handleUpdateQuestionSet } from './updateSet';
+import { handleDeleteQuestionSet } from './deleteSet';
 
 const router = express.Router();
 
@@ -10,6 +11,7 @@ router.get('/mine', authMiddleware, handleGetMySets);
 router.get('/:id', authMiddleware, handleGetSetById);
 router.post('/', authMiddleware, handleCreateQuestionSet);
 router.put('/', authMiddleware, handleUpdateQuestionSet);
+router.delete('/:id', authMiddleware, handleDeleteQuestionSet);
 
 // Export the router
 export default router;
