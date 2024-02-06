2f50c02d654d82a029d415ce28cb81cb4f65f15b
caca872 <caca872@vidalibarraquer.net>
2024-02-06 13:43:37 +0100

    Modified create test page

--- src/questions/create.tsx
+++ src/questions/create.tsx
@@ -1,4 +1,4 @@
-import { XIcon } from 'lucide-react'
+import { PlusIcon, XIcon } from 'lucide-react'
 import React from 'react'
 import useQuestion from '@/hooks/useQuestion'
 import FileUpload from '@/components/file-upload'
@@ -8,53 +8,49 @@ import useRaisedShadow from '@/hooks/useRaisedShadow'
 type Props = {}
 
 export default function Create({ }: Props) {
-    const { questions, addQuestion, updateQuestion, removeQuestion, setQuestions } = useQuestion()
-    const [headerFile, setHeaderFile] = React.useState<File | null>(null)
-    const y = useMotionValue(0);
-    const boxShadow = useRaisedShadow(y);
+    const { questions, addQuestion, updateQuestion, removeQuestion, setQuestions } = useQuestion(Array.from({ length: 24 }).map(_ => ({
+        question: 'A company is planning to run a global marketing application in the AWS Cloud. The application will feature videos that can be viewed by users. The company must ensure that all users can view these videos with low latency.\nWhich AWS service should the company use to meet this requirement?',
+        solution: 1,
+        answers: []
+    })))
+    const [selectedQuestion, setSelectedQuestion] = React.useState<number | null>(questions.length === 0 ? null : 0)
 
     return (
-        <div className='w-2/3 flex flex-col gap-3 z-50 justify-start items-start pt-24 mx-auto pb-4'>
-            <div className='text-4xl font-bold'>New Test</div>
-            <FileUpload />
-            <motion.div className='w-full flex flex-col gap-4'>
-                <AnimatePresence>
+        <>
+            <div className='flex flex-col absolute max-h-[calc(100vh - 4 rem)] gap-2 overflow-y-auto text-white pt-16 px-4 inset-0 w-96 border-r border-r-white'>
+                <button onClick={() => { addQuestion(); setSelectedQuestion(questions.length - 1) }} className='w-full bg-transparent py-2 text-emerald-500 hover:bg-emerald-500/20 ring ring-emerald-500 rounded-md'>
+                    <PlusIcon className='w-6 h-6 m-auto' />
+                </button>
+                <div className={`${questions.length > 25 ? "text-red-500" : ""} text-center py-1`}>{questions.length} / 25 Questions</div>
+                {
+                    questions.map((question, index) => (
+                        <div onClick={() => setSelectedQuestion(index)} className={`text-gray-300 group cursor-pointer relative hover:bg-gray-600 p-2 rounded-md ${selectedQuestion === index ? "border border-gray-700 bg-gray-800" : ""}`} key={index}>
+                            {question.question.slice(0, 70)}...
+                            <button onClick={() => removeQuestion(index)} className='hidden group-hover:block absolute top-2 right-2'>
+                                <XIcon className='w-6 h-6 text-red-500' />
+                            </button>
+                        </div>
+                    ))
+                }
+            </div>
+            <div className='pl-96 w-2/3 mx-auto h-screen flex flex-col gap-4 justify-center items-center'>
+                <input
+                    placeholder='Question' value={selectedQuestion ? questions[selectedQuestion].question : ""}
+                    onChange={e => selectedQuestion && updateQuestion(selectedQuestion, { ...questions[selectedQuestion], question: e.currentTarget.value })}
+                    type="text"
+                    className='w-full p-2 bg-transparent border-b border-b-white focus:ring-0 text-white text-lg focus:outline-none' />
+                <div className='flex flex-col gap-2 justify-center items-start w-full'>
                     {
-                        questions.map((question, i) => (
-                            <motion.div style={{ boxShadow, y }} key={question.question} id={question.question} className='flex relative flex-col gap-3 p-3 bg-white bg-opacity-75 rounded-lg border drop-shadow-lg w-full'>
-                                <button onClick={() => removeQuestion(i)} className='absolute -right-4 -top-4 w-10 h-10 rounded-full bg-red-500 flex justify-center items-center text-white'>
-                                    <XIcon />
-                                </button>
-                                <input type="file" name="img" id="img" />
-                                <input tabIndex={i * 5 + 1} type="text" placeholder='Question' className='mt-2 text-lg px-3 py-1 bg-transparent border-b border-b-black' value={question.question} onChange={(e) => updateQuestion(i, { ...question, question: e.currentTarget.value })} />
-                                {
-                                    question.answers.map((answer, j) => (
-                                        <div className='flex items-center gap-2 py-2'>
-                                            <input tabIndex={-1} type="radio" name={`question-${i}`} id={`question-${i}-answer-${j}`} />
-                                            <input
-                                                tabIndex={(i * 5) + (j + 2)}
-                                                type="text"
-                                                placeholder='Answer'
-                                                value={answer}
-                                                onChange={(e) => {
-                                                    const newQuestion = { ...question }
-                                                    newQuestion.answers[j] = e.currentTarget.value
-                                                    updateQuestion(i, newQuestion)
-                                                }}
-                                                className='w-full py-1 px-2 bg-opacity-0 bg-transparent border-b active:border-b border-b-black ' />
-                                        </div>
-                                    ))
-                                }
-                            </motion.div>
+                        Array.from({ length: 4 }).map((_, index) => (
+                            <div key={`sol_${index}`} className='flex gap-3 w-full'>
+                                <input type="radio" name="solution" id={`sol_${index}`} checked={selectedQuestion === index ? true : false} onChange={() => selectedQuestion && updateQuestion(selectedQuestion, { ...questions[selectedQuestion], solution: index })} className='w-6' />
+                                <input type="text" name="solution-text" id={`sol_${index}_text`} className='w-full py-1 px-2 bg-transparent border-b border-b-white focus:ring-0 text-white focus:outline-none' />
+                            </div>
                         ))
                     }
-                </AnimatePresence>
-            </motion.div>
-            <div className='flex flex-row gap-2 justify-end items-center w-full mt-4'>
-                <button className=' min-w-36 px-4 py-2 bg-yellow-50 hover:bg-yellow-200 font-medium text-black border border-black rounded-lg' onClick={addQuestion}>Add Question</button>
-                <button className=' min-w-36 px-4 py-2 bg-slate-900 text-white font-medium border border-black rounded-lg'>Create Test</button>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
