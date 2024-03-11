f1693ec4d267dbceb0d38e2606d98895c515bce2
Albert Bru Corts <albertbru04@gmail.com>
2024-03-11 09:15:38 +0100

    Removed console.logs statements

--- backend/ws/events/joinGame.ts
+++ backend/ws/events/joinGame.ts
@@ -13,16 +13,10 @@ export default async function joinGame(data: string, socket: Socket) {
     await socket.join(dataJ.gameId as string);
     console.log(`User ${dataJ.name} joined game ${dataJ.gameId}`);
     let id = running.joinGame(dataJ.gameId, dataJ.name);
-    console.log({ id})
     socket.emit("joinedGame", JSON.stringify({
         currentUser: { id, name: dataJ.name},
         participants: running.getPlayers(dataJ.gameId),
-    }
-    ));
-    console.log({
-        currentUser: { id, name: dataJ.name},
-        participants: running.getPlayers(dataJ.gameId).map(x => ({ id: x.id, name: x.name }) ),
-    })
+    }));
     socket.to(dataJ.gameId as string).emit("currentPlayers", JSON.stringify(running.getPlayers(dataJ.gameId).sort((a, b) => a.id - b.id)));
     console.log(running.getPlayers(dataJ.gameId))
 }
