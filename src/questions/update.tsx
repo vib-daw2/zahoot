bc64858b9993d31a1b5cdc8b74dea6cce0f5a56f
AagieKLZ <darkyelan@gmail.com>
2024-03-18 14:03:15 +0100

    Fixed update questions interactions

--- src/questions/update.tsx
+++ src/questions/update.tsx
@@ -78,9 +78,9 @@ export default function UpdateQuestions({ action = "create" }: Props) {
                 <div className={`${questions.length > 25 ? "text-red-500" : ""} text-center py-1`}>{questions.length} / 25 Questions</div>
                 {
                     questions.map((question, index) => (
-                        <div onClick={() => setSelectedQuestion(index)} className={`text-gray-300 group cursor-pointer relative hover:bg-gray-600 p-2 pr-6 rounded-md ${selectedQuestion === index ? "border border-gray-700 bg-gray-800" : ""}`} key={index}>
-                            {question.question.slice(0, 70)}{question.question.length > 70 && "..."}
-                            <button onClick={() => deleteQuestion(index)} className='hidden group-hover:block absolute top-2 right-2'>
+                        <div onClick={() => setSelectedQuestion(index)} className={`text-gray-300 group w-full z-50 cursor-pointer relative hover:bg-gray-600 p-2 pr-6 rounded-md ${selectedQuestion === index ? "border border-gray-700 bg-gray-800" : ""}`} key={index}>
+                            <div>{question.question.slice(0, 70)}{question.question.length > 70 && "..."}</div>
+                            <button onClick={() => deleteQuestion(index)} className='hidden w-fit h-fit group-hover:block absolute top-2 right-2'>
                                 <XIcon className='w-6 h-6 text-red-500' />
                             </button>
                         </div>
@@ -88,32 +88,35 @@ export default function UpdateQuestions({ action = "create" }: Props) {
                 }
             </div>
             <SaveDialog defaultName={setName} defaultDescription={setDescription} id={id} />
-            {selectedQuestion !== null && <div className='pl-96 relative w-2/3 mx-auto h-screen flex flex-col gap-4 justify-center items-center'>
-                <input
-                    placeholder='Question' value={selectedQuestion !== null ? questions[selectedQuestion]?.question : ""}
-                    onChange={e => selectedQuestion !== null && updateQuestion(selectedQuestion, { ...questions[selectedQuestion], question: e.currentTarget.value })}
-                    type="text"
-                    className='w-full p-2 bg-transparent border-b border-b-white focus:ring-0 text-white text-lg focus:outline-none' />
-                <div className='flex flex-col gap-2 justify-center items-start w-full'>
-                    {
-                        Array.from({ length: 4 }).map((_, index) => (
-                            <div key={`sol_${index}`} className='flex gap-3 w-full'>
-                                <input type="radio" name="solution" id={`sol_${index}`} checked={questions[selectedQuestion].solution === index ? true : false} onChange={() => selectedQuestion !== null && updateQuestion(selectedQuestion, { ...questions[selectedQuestion], solution: index })} className='w-6' />
-                                <input
-                                    type="text"
-                                    name="solution-text"
-                                    id={`sol_${index}_text`}
-                                    value={questions[selectedQuestion]?.answers[index] ?? ""}
-                                    onChange={e => {
-                                        const newAnswers = questions[selectedQuestion].answers
-                                        newAnswers[index] = e.currentTarget.value
-                                        updateQuestion(selectedQuestion, { ...questions[selectedQuestion], answers: newAnswers })
-                                    }}
-                                    className='w-full py-1 px-2 bg-transparent border-b border-b-white focus:ring-0 text-white focus:outline-none'
-                                />
-                            </div>
-                        ))
-                    }
+            {selectedQuestion !== null && <div className='ml-96 relative w-[calc(100%-24rem)] px-8 z-50 mx-auto h-screen flex flex-col gap-4 justify-center items-center'>
+                <div className='mx-auto w-full flex flex-col justify-center items-center gap-3 max-w-3xl'>
+
+                    <input
+                        placeholder='Question' value={selectedQuestion !== null ? questions[selectedQuestion]?.question : ""}
+                        onChange={e => selectedQuestion !== null && updateQuestion(selectedQuestion, { ...questions[selectedQuestion], question: e.currentTarget.value })}
+                        type="text"
+                        className='w-full p-2 bg-transparent border-b border-b-white focus:ring-0 text-white text-lg focus:outline-none' />
+                    <div className='flex flex-col gap-2 justify-center items-start w-full'>
+                        {
+                            Array.from({ length: 4 }).map((_, index) => (
+                                <div key={`sol_${index}`} className='flex gap-3 w-full'>
+                                    <input type="radio" name="solution" id={`sol_${index}`} checked={questions[selectedQuestion].solution === index ? true : false} onChange={() => selectedQuestion !== null && updateQuestion(selectedQuestion, { ...questions[selectedQuestion], solution: index })} className='w-6' />
+                                    <input
+                                        type="text"
+                                        name="solution-text"
+                                        id={`sol_${index}_text`}
+                                        value={questions[selectedQuestion]?.answers[index] ?? ""}
+                                        onChange={e => {
+                                            const newAnswers = questions[selectedQuestion].answers
+                                            newAnswers[index] = e.currentTarget.value
+                                            updateQuestion(selectedQuestion, { ...questions[selectedQuestion], answers: newAnswers })
+                                        }}
+                                        className='w-full py-1 px-2 bg-transparent border-b border-b-white focus:ring-0 text-white focus:outline-none'
+                                    />
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
             </div>}
         </>
