978f556de8f49142471fffb6e9683c66c9e2e922
Albert Bru Corts <albertbru04@gmail.com>
2024-04-16 10:50:25 +0200

    Update profile route POST /api/profile

--- backend/routes/profile/router.ts
+++ backend/routes/profile/router.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import authMiddleware from '../../middleware/auth';
 import handleStats from './stats';
+import handleUpdate from './update';
 
 const router = express.Router();
 
+router.post('/', authMiddleware, handleUpdate);
 router.get('/stats', authMiddleware, handleStats);
 
 // Export the router
