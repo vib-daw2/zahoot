7f49e49fa5d084257abad881958a284bffd753d3
Albert Bru Corts <albertbru04@gmail.com>
2024-04-16 09:39:33 +0200

    Refactor WebSocket handling and add solution and nextQuestion events

---
+++ backend/ws/events/nextQuestion.ts
@@ -0,0 +1,29 @@
+import { Socket } from "socket.io";
+import running from "../ongoing/games";
+import io from "../../server";
+
+export default async function nextQuestion(data: string, socket: Socket) {
+    const { gameId }: { gameId: string } = JSON.parse(data);
+
+    const roundCompleted = running.roundCompleted(gameId) // Comprueba si todos los jugadores han respondido
+
+    const game = running.getGame(gameId) 
+    if (!game) return
+
+    const hasMoreQuestions = (running.getGame(gameId)?.currentQuestion ?? 1) < (running.getGame(gameId)?.questions.length ?? 0)
+    if (roundCompleted && hasMoreQuestions) {
+        const nextQuestion = running.nextQuestion(gameId)
+        console.log({ nextQuestion })
+        io.to(gameId).emit("nextQuestion", JSON.stringify(running.nextQuestion(gameId)))
+    } else if (!roundCompleted && hasMoreQuestions) {
+        const unanswered = running.getPlayers(gameId).filter(x => x.responses.length < game.currentQuestion)
+        unanswered.forEach(x => {
+            game.players.find(y =>
+                y.socketId === x.socketId
+            )?.responses.push(false)
+        })
+        io.to(gameId).emit("nextQuestion", JSON.stringify(running.nextQuestion(gameId)))
+    } else if (!hasMoreQuestions) {
+        io.to(gameId).emit("gameEnd")
+    }
+}
---
+++ backend/ws/events/solution.ts
@@ -0,0 +1,19 @@
+import { Socket } from "socket.io";
+import running from "../ongoing/games";
+import io from "../../server";
+
+export default async function solution(data: string, socket: Socket) {
+    const response = JSON.parse(data) as { gameId: string, questionId: number, solution: number, userId: number }
+    running.addResponse(response.gameId,response.userId, response.solution)
+    const players = running.getPlayers(response.gameId)
+    
+    // Envía la solución correcta a los jugadores que ya han respondido
+    const game = running.getGame(response.gameId)
+    if (!game) return
+    const currentQuestion = game.currentQuestion
+    const nonResponded = players.filter(x => x.responses.length < currentQuestion + 1)
+    const correctSolution = game.questions[currentQuestion].choices.find(x => x.isCorrect)
+    if (!correctSolution) return
+    const correctSolutionIndex = game.questions[currentQuestion].choices.indexOf(correctSolution)
+    io.to(response.gameId).except(nonResponded.map(x => x.socketId)).emit("correctSolution", JSON.stringify(correctSolutionIndex))
+}
