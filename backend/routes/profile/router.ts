841c9b94cb3e0549c73728a885a340bcc9e7174f
Albert Bru Corts <albertbru04@gmail.com>
2024-04-16 13:49:02 +0200

    GET /profile

--- backend/routes/profile/router.ts
+++ backend/routes/profile/router.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import authMiddleware from '../../middleware/auth';
 import handleStats from './stats';
 import handleUpdate from './update';
+import handleGetMyProfile from './get';
 
 const router = express.Router();
 
+router.get('/', authMiddleware, handleGetMyProfile);
 router.post('/', authMiddleware, handleUpdate);
 router.get('/stats', authMiddleware, handleStats);
 
