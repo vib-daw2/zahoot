822af1ebcc2a74430c0b481ca8b2262ce9688b03
AngeLauri1501 <an1a223@vidalibarraquer.net>
2024-05-14 12:54:19 +0200

    Added socket url

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -4,4 +4,4 @@ import { io } from 'socket.io-client';
 const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
 
 //export const socket = io("http://localhost:3000");
-export const socket = io("http://192.168.85.8:3000/")
+export const socket = io(import.meta.env.VITE_SOCKET_URL ?? "http://192.168.85.8:3000/")
